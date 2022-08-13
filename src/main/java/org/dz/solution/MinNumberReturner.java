fccb8a1da8ec92dfd54b6c11188959a3e7ac9d20
anassvoronn <UQzbwQCt7R3uMfz>
2022-08-13 13:38:27 +0300

    MinNumberReturner

--- src/main/java/org/dz/solution/MinNumberReturner.java
+++ src/main/java/org/dz/solution/MinNumberReturner.java
@@ -4,17 +4,16 @@ public class MinNumberReturner {
     public double getMinimalNumber(final int intNumber,
                                    final float floatNumber,
                                    final double doubleNumber) {
-        if ((Math.abs(intNumber)) < (Math.abs(floatNumber)) && (Math.abs(intNumber)) < (Math.abs(doubleNumber))) {
-            System.out.println(Math.abs(intNumber));
+        final int intNumberAbs = Math.abs(intNumber);
+        final float floatNumberAbs = Math.abs(floatNumber);
+        final double doubleNumberAbs = Math.abs(doubleNumber);
+
+        if (intNumberAbs < floatNumberAbs && intNumberAbs < doubleNumberAbs) {
             return intNumber;
-        } else if ((Math.abs(floatNumber)) < (Math.abs(intNumber)) && (Math.abs(floatNumber)) < (Math.abs(doubleNumber))) {
-            System.out.println(Math.abs(floatNumber));
+        } else if (floatNumberAbs < intNumberAbs && floatNumberAbs < doubleNumberAbs) {
             return floatNumber;
         } else {
-            System.out.println(Math.abs(doubleNumber));
             return doubleNumber;
         }
-
     }
 }
-
