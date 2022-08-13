210005d5c9c0da3b1a68482375cdf4466eb57b6a
anassvoronn <UQzbwQCt7R3uMfz>
2022-08-13 10:29:08 +0300

    MinNumberReturner

--- src/main/java/org/dz/solution/MinNumberReturner.java
+++ src/main/java/org/dz/solution/MinNumberReturner.java
@@ -2,18 +2,19 @@ package org.dz.solution;
 
 public class MinNumberReturner {
     public double getMinimalNumber(final int intNumber,
-                                 final float floatNumber,
-                                 final double doubleNumber) {
+                                   final float floatNumber,
+                                   final double doubleNumber) {
         if ((Math.abs(intNumber)) < (Math.abs(floatNumber)) && (Math.abs(intNumber)) < (Math.abs(doubleNumber))) {
-            System.out.println (Math.abs(intNumber));
+            System.out.println(Math.abs(intNumber));
             return intNumber;
         } else if ((Math.abs(floatNumber)) < (Math.abs(intNumber)) && (Math.abs(floatNumber)) < (Math.abs(doubleNumber))) {
-            System.out.println (Math.abs(floatNumber));
+            System.out.println(Math.abs(floatNumber));
             return floatNumber;
         } else {
-            System.out.println (Math.abs(doubleNumber));
+            System.out.println(Math.abs(doubleNumber));
             return doubleNumber;
         }
 
     }
 }
+
